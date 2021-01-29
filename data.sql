f928ab8de21e1a68654638299f35cb210784dc36
Tristan <ttrosvig804@gmail.com>
2021-01-29 12:06:17 -0600

    added folders table and folder_id field to todo table

--- data.sql
+++ data.sql
@@ -2,8 +2,14 @@
 
 DROP TABLE IF EXISTS todos;
 
+CREATE TABLE folders (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
+  folder_id INTEGER NOT NULL REFERENCES folders,
   description TEXT,
   completed TEXT
 );
